537f2ba9d68cd74c748fc55c5b61ea78fc2ac8eb
rahulholkar16 <usefulltech1819@gmail.com>
2026-05-28 22:43:55 +0530

    Add more types

--- src/types/index.d.ts
+++ src/types/index.d.ts
@@ -1,13 +1,35 @@
 interface IApiErrors {
     statusCode: number,
-    message: string,
+    message?: string,
     success: boolean,
     errors?: unknown
 }
 
 interface IApiResponse <T = unknown> {
     statusCode: number,
-    message: string,
+    message?: string,
     success: boolean,
     data?: T
+}
+
+export type Language = 'python' | 'javascript';
+
+export type Status = 'pending' | 'running' | 'done' | 'error' | 'timeout';
+
+export interface SubmissionJob {
+    token: string;
+    code: string;
+    language: Language;
+}
+
+export interface SubmissionResult {
+    status: Status;
+    stdout?: string;
+    stderr?: string;
+    exit_code?: number;
+}
+
+export interface SubmitRequestBody {
+    code: string;
+    language: Language;
 }
